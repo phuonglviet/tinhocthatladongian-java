5990bf0d9acd1dffab2e024db6bc8beeb70c6df3
phuongpnl <phuong.le@playnext-lab.co.jp>
2021-05-18 23:20:39 +0900

    edit he lo

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello Tin Hoc That La Don Gian <br> Tokyo");
+		get("/hello", (req, res) -> "Hello Tin Hoc That La Don Gian <br> Tokyo <br> Osaka <br>");
 	}
 }
